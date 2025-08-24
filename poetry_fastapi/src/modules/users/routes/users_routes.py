7a847ea3c6dc54239eb9af12ff1aff1b8be4d04f
dheerendra143 <singhdheerendra83@gmail.com>
2025-08-24 21:19:10 +0530

    Added request api payload validation

--- poetry_fastapi/src/modules/users/routes/users_routes.py
+++ poetry_fastapi/src/modules/users/routes/users_routes.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
+from starlette import status
+
 from ..controllers.users_controllers import create_users_ctrl
 from ..validations.create_user import Usercreate
 
 routers = APIRouter()
 
 
-@routers.post("/")
+@routers.post("/", status_code= status.HTTP_201_CREATED)
 def create_users(user_request: Usercreate):
     response = create_users_ctrl(user_request)
     return response
